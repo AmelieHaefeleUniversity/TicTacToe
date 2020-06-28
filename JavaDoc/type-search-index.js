366a8340ebd9134b56d53b0939308b6a98d00605
Amelie Haefele <amelie@hafele.org>
2020-06-27 21:19:39 -0400

    Improved code structure after code review. Added features to go for win, or block if it can. Improved testing.

--- JavaDoc/type-search-index.js
+++ JavaDoc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.company","l":"Main"},{"p":"com.company","l":"movesOnBoard"},{"p":"com.company","l":"movesOnBoardTest"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.company","l":"BoardSquare"},{"p":"com.company","l":"Line"},{"p":"com.company","l":"Main"},{"p":"com.company","l":"Tests"},{"p":"com.company","l":"TicTacToeGame"}]
